38778bfef7821d987b407adcdf5f78e4f3220a7d
Maxime Huyghe <maxhuy@laposte.net>
2022-05-04 10:17:18 +0200

    feature: json cache mapping patent names to (date, keywords)

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 import csv
+import os
+import json
+from typing import Dict
 from alive_progress import alive_bar
 
 
 def main():
     # load the last 200 patents
     patents = get_patents(200)
-    for patent in patents:
-        print(patent)
-        print("\n")
+    print(patents)
+
+
+JSON_CACHE_PATH = "ibm_patents/patent_metadata_with_keywords.json"
+ORIGINAL_PATENTS_PATH = "ibm_patents/patent_metadata.csv"
 
 
 def get_patents(n_to_get):
     """
     Loads the last `n_to_get` patents from the filesystem.
     """
-    with open("ibm_patents/patent_metadata.csv") as file:
+    with open(ORIGINAL_PATENTS_PATH) as file:
         reader = csv.DictReader(file)
         patents = list(reader)
+    patents_with_keywords: Dict = {}
+    # a+ = append or create, can also read
+    if os.path.exists(JSON_CACHE_PATH):
+        with open(JSON_CACHE_PATH, "r") as file:
+            patents_with_keywords = json.load(file)
+    else:
+        with open(JSON_CACHE_PATH, "w") as file:
+            json.dump({}, file)
     patents.sort(key=lambda p: p["Date_Filed"], reverse=True)
-    patents = patents[:n_to_get]
-    # print(list(map(lambda p: p["Date_Filed"], metadata[:10])))
-    with alive_bar(n_to_get) as progress_bar:
-        for patent_metadata in patents:
-            # replace .pdf with .txt
-            patent_metadata["Name"].split(".")[0] + ".txt"
-            with open("ibm_patents/patent_metadata.csv") as patent_file:
-                patent_metadata["Keywords"] = truc_pyate(patent_file.read())
+    patents_to_process = patents[:n_to_get]
+    # This is here because we don't want to return everything in `patents_with_keywords`,
+    # but we still want to keep it to write it back to disk.
+    patents_with_keywords_to_return = {}
+    with alive_bar(len(patents_to_process)) as progress_bar:
+        for patent_metadata in patents_to_process:
+            patent_name = patent_metadata["Name"]
+            if patent_name not in patents_with_keywords:
+                # replace .pdf with .txt
+                # filename = patent_metadata["Name"].split(".")[0] + ".txt"
+                # with open("ibm_patents/texts/texts/" + filename) as patent_file:
+                patent = {"date": patent_metadata["Date_Filed"], "keywords": ["TODO"]}
+                patents_with_keywords_to_return[patent_name] = patent
+                patents_with_keywords[patent_name] = patent
+            else:  # read from cache
+                patents_with_keywords_to_return[patent_name] = patents_with_keywords[
+                    patent_name
+                ]
             progress_bar()
-    return patents
+    with open(JSON_CACHE_PATH, "w") as file:
+        json.dump(patents_with_keywords, file)
+    return patents_with_keywords_to_return
 
 
 if __name__ == "__main__":
